b16c44ca82ea7ba3ef14b09830927d1db1eb5aea
Marelons1337 <jarorafo@gmail.com>
2020-10-06 22:46:42 +0200

    Updates

---
+++ precache-manifest.57948aa4e1daf6edb518f211f5c11c11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a3987c839746e3808dcc8655d311e0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "50c727064ed47b4db5e1",
+    "url": "/robofriends/static/css/main.4461a1cd.chunk.css"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robofriends/static/js/2.14bc996e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.14bc996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c727064ed47b4db5e1",
+    "url": "/robofriends/static/js/main.9f28432b.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
